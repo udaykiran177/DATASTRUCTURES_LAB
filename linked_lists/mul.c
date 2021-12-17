a9bd60c983a53c572ec4297a403ef0e13b4cec95
udaykiran177 <95631110+udaykiran177@users.noreply.github.com>
2021-12-17 10:32:12 +0530

    multiplication of polynomials

---
+++ linked_lists/mul.c
@@ -0,0 +1,25 @@
+void mul(int n)
+{
+        temp1=head1;
+        temp2=head2;
+        /*while(temp1&&temp2)
+        {
+
+                for (int i=0;i<n;i++)
+                {
+                        printf("%d x ^ %d",temp1->quot*temp2->quot,temp1->deg+temp2-deg);
+                        temp1=temp1->next;
+                }
+                temp2=temp2->next;
+
+        }*/
+        while (temp1!=null)
+        {
+                while (temp2!=null)
+                {
+                        printf("%d X ^ %d",temp1->quot*temp2->quot,temp1->deg+temp2->deg);
+                        temp2=temp2->next;
+                }
+                temp1=temp1->next;
+        }
+}
